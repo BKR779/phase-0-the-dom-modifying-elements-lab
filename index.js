9c628ab833e68a93589d0fc7ca384ce5d4d2077f
Abobakr Mahdi <93738112+BKR779@users.noreply.github.com>
2023-03-02 00:21:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+// Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = `Ahmad of the House AlRai, First of his name. King of the Andals, of the Rhoynar, and of the First Men. Lord of the Seven Kingdoms and Protector of the Realm; is the champion`;
